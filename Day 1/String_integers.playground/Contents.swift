a529c44ba486929c08342f6ebc1432ca50e72ef5
Anthony Beckford <abeckford532@gmail.com>
2022-09-10 07:18:43 -0400

    Add files via upload
    Day 1 – variables, simple data types, and string interpolation

---
+++ Day 1/String_integers.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+// This is how you create an integer
+var age = 38
+
+// This is how you create large number integers
+var population = 8_000_000
+
+
+// Creates a variable called str
+var str = "Hello, playground"
+
+// str is a variable  so we can change it
+str = "goodbye"
+
+
+
+// Multi-line Strings
+
+var str1 = """
+this goes
+over multiple
+line
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and Booleans
+
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+
+var score = 85
+var str = "Your score was \(score)"
+
+// Constants
+
+//Values that are set once and then never changed again
+let taylor = "swift"
+
+
+// Type annotations
+
+
