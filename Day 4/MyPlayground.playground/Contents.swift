897e1362635005bbf16de5a2c86ced91e792f319
Anthony Beckford <abeckford532@gmail.com>
2022-09-19 21:14:18 -0400

    Update Contents.swift

--- Day 4/MyPlayground.playground/Contents.swift
+++ Day 4/MyPlayground.playground/Contents.swift
@@ -7,3 +7,43 @@ let count = 1...10
 for number in count {
     print("Number is \(count)")
 }
+
+
+// while loop
+var number = 1
+
+while number <= 20 {
+    print(number)
+    number += 1
+}
+
+print("Ready or not here I come!")
+
+
+// Repeat Loop
+
+var number = 1
+
+repeat {
+    print(number)
+    number += 1
+} while number <= 20
+            
+print("Read or not, here I come!")
+
+
+// Exiting Loops
+
+var countdown = 10
+
+while countdown >= 0 {
+    print(countdown)
+   
+    if countdown == 4 {
+        print("I'm bored. Let's go now!")
+        break
+    }
+    countdown -= 1
+        
+}
+
